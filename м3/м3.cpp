0d9b0a542dcefd5926e22c42120879845e8c930f
Lenovo <Lenovo@DESKTOP-RE9Q0P4>
2024-03-19 11:10:12 +0200

    Add project files.

---
+++ м3/м3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    int n;
+    cout << "Введiть номер числа Фiбоначчi: ";
+    cin >> n;
+
+    // che
+    if (n < 0) {
+        cout << "Введiть додатне цiле число." << endl;
+    }
+    else {
+        cout << n << "-е число Фiбоначчi: " << fibonacci(n) << endl;
+    }
+
+    return 0;
+}
